c15fbdc9c6960bf65006375a91ffbf0b3776af71
jesper.Helsltrom <jesper.hellstrm@gmail.com>
2025-01-06 16:07:03 +0100

    Initial commit

---
+++ SmartMealCalculatorServer/DB/ingredientsDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartMealCalculatorServer.DB
+{
+    public class ingredientsDBContext : DbContext
+    {
+        public DbSet<string> Test {  get; set; }
+    }
+}
