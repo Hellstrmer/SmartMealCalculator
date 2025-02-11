861da48afddc81d4f4c4bfa87ddb4f57c7a69dbf
Hellstrmer <jesper.hellstrm@gmail.com>
2025-02-11 18:45:24 +0100

    fixed appsettings

--- SmartMealCalculator/Program.cs
+++ SmartMealCalculator/Program.cs
@@ -3,10 +3,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartMealCalculator;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-Console.WriteLine("Hello from Blazor!"); // <-- Try this
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-Console.WriteLine("Blazor-appen startar...");
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.Configuration["API_BASE_URL"];
 Console.WriteLine($"ApiBaseUrl: {apiBaseUrl}");
 
