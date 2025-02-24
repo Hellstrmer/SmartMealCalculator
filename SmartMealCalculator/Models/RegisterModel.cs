2a0a056fd2467bd58ae1820cf5ad86e71e51dff2
jesper.Helsltrom <jesper.hellstrm@gmail.com>
2025-02-24 08:09:00 +0100

    small identity fixes

---
+++ SmartMealCalculator/Models/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartMealCalculator.Models
+{
+    public class RegisterModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        
+    }
+}
---
+++ SmartMealCalculator/Services/LoginService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Json;
+
+namespace SmartMealCalculator.Services
+{
+    public class LoginService
+    {
+        private readonly HttpClient _httpClient;
+        public LoginService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task RegisterAsync()
+        {
+            var result = await _httpClient.PostAsJsonAsync(
+                "register", new
+                {
+                    email,
+                    password
+                });
+        }
+    }
+}
