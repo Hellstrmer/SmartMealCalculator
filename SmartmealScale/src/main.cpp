5b61ace7a6bedbd71e7e4d1be3a26a62bd7e0390
Hellstrmer <jesper.hellstrm@gmail.com>
2025-03-03 20:38:22 +0100

    Added Websocket to ESP-program

--- SmartmealScale/src/main.cpp
+++ SmartmealScale/src/main.cpp
@@ -1,55 +1,70 @@
 #include <Arduino.h>
-#include <ArduinoWebsockets.h>
+#include <WebSocketsClient_Generic.h>
 #include <wifi.h>
 #include <config.h>
+#include <HTTPClient.h>
 
-const uint16_t websockets_server_port = 5099;
+WebSocketsClient client;
+String handshake = "{\"protocol\":\"json\",\"version\":1}";
+unsigned long previousMillis = 0;
+const long interval = 2000;
 
-using namespace websockets;
-
-void onMessageCallback(WebsocketsMessage message) {
-  Serial.print("Got Message: ");
-  Serial.print("message.data()");
-}
-
-void onEventCallback(WebsocketsEvent event, String data) {
-
-  if(event == WebsocketsEvent::ConnectionOpened){
-    Serial.println("Connection Opened");
-  } else if (event == WebsocketsEvent::ConnectionClosed){
-    Serial.println("Connection Closed");
-  } else if (event == WebsocketsEvent::GotPing) {
-    Serial.println("Got a ping!");
-  } else if (event == WebsocketsEvent::GotPong) {
-    Serial.println("Got a pong!");
+void WebsocketEvent(WStype_t type, uint8_t* payload, size_t length){
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.println("Disconnected from WebSocket server");
+      break;
+    case WStype_CONNECTED:
+      Serial.println("Connected to WebSocket server");      
+      client.sendTXT(handshake);
+      delay(1000);
+      
+      break;
+    case WStype_TEXT:
+      Serial.printf("Received message: %s\n", payload);
+      break;
   }
 }
 
-WebsocketsClient client;
+void SendMessage(String message){
+  String SignalRMessage = String("{\"arguments\":[\"") + message + "\"],\"invocationId\":\"0\",\"target\":\"SendMessage\",\"type\":1}";
+  client.sendTXT(SignalRMessage);
+}
 
-void setup() {
+
+void setup()
+{
   Serial.begin(9600);
   WiFi.begin(ssid, password);
 
-  for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++){
-    Serial.print(".");
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
+    Serial.print(".");
   }
+  Serial.println("\nConnected to Wi-Fi!");
 
-  client.onMessage(onMessageCallback);
 
-  // Run Callback when events are occuring
-  client.onEvent(onEventCallback);
+  client.begin("192.168.50.51", 5099, "/weight");
+  client.onEvent(WebsocketEvent);
 
-  // Connect to server
-  client.connect(websocket_server_host);
-
-  client.send("Hello from Esp!");
-
-  client.ping();
 }
 
-void loop() {
-  client.poll();
+void loop()
+{
+  client.loop();
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    static int messageCount = 0;
+    if (messageCount == 0) {
+      SendMessage("Hej!");
+    } else if (messageCount == 1) {
+      SendMessage("Funkar va?");
+    }
+    messageCount = (messageCount + 1) % 2;
+  }  
 }
+
 
