455bffce141b44a453dd62ced7a20651c7c03e90
Hellstrmer <jesper.hellstrm@gmail.com>
2025-03-05 07:09:37 +0100

    Added HX711

--- SmartmealScale/src/main.cpp
+++ SmartmealScale/src/main.cpp
@@ -3,11 +3,31 @@
 #include <wifi.h>
 #include <config.h>
 #include <HTTPClient.h>
+#include <HX711.h>
 
+//Buttons
+
+const int touchPin = T7;
+const int touchPowerTreshold = 80;
+const int UnitPin = T9;
+const int touchUnitTreshold = 80;
+const int TarePin = T5;
+const int touchTareTreshold = 80;
+int previousTouch;
+
+
+//SignalR
 WebSocketsClient client;
 String handshake = "{\"protocol\":\"json\",\"version\":1}";
+
+//HX711
+const int LoadCell_DOUT_Pin = 16;
+const int LoadCell_SCK_Pin = 4;
+HX711 scale;
+
+//Delay
 unsigned long previousMillis = 0;
-const long interval = 2000;
+const long interval = 200;
 
 void WebsocketEvent(WStype_t type, uint8_t* payload, size_t length){
   switch (type) {
@@ -32,8 +52,10 @@ void SendMessage(String message){
 }
 
 
+
 void setup()
 {
+  //Wifi
   Serial.begin(9600);
   WiFi.begin(ssid, password);
 
@@ -45,26 +67,57 @@ void setup()
   }
   Serial.println("\nConnected to Wi-Fi!");
 
-
+//SignalR
   client.begin("192.168.50.51", 5099, "/weight");
   client.onEvent(WebsocketEvent);
 
+//HX711
+scale.begin(LoadCell_DOUT_Pin, LoadCell_SCK_Pin);
 }
 
 void loop()
 {
+
   client.loop();
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval){
-    previousMillis = currentMillis;
-    static int messageCount = 0;
-    if (messageCount == 0) {
-      SendMessage("Hej!");
-    } else if (messageCount == 1) {
-      SendMessage("Funkar va?");
-    }
-    messageCount = (messageCount + 1) % 2;
-  }  
+  // if (currentMillis - previousMillis >= interval){
+  //   previousMillis = currentMillis;
+  //   static int messageCount = 0;
+  //   if (messageCount == 0) {
+  //       Screen();
+  //   }
+  //   // } else if (messageCount == 1) {
+  //   //   analogWrite(displayPin, 0);
+  //   // }
+  //  messageCount = (messageCount + 1) % 2;
+ // }  
+  int touchValuePowerButton = touchRead(touchPin);
+  int touchValueUnitButton = touchRead(UnitPin);
+  int touchValueTareButton = touchRead(TarePin);
+  bool PowerOn;
+  bool Tare;
+  bool Unit;
+
+  if (touchValuePowerButton < touchPowerTreshold && previousTouch == 0){
+    PowerOn = true;
+    SendMessage("Startad!");
+    previousTouch = touchValuePowerButton;
+  }  else if (touchValueUnitButton < touchUnitTreshold && previousTouch == 0){
+    Tare = true;
+    SendMessage("Unit");
+  } else if (touchValueTareButton < touchTareTreshold && previousTouch == 0){
+    Unit = true;
+    SendMessage("Unit");
+  } else if (touchValuePowerButton > touchPowerTreshold
+    && touchValueUnitButton > touchUnitTreshold  
+    && touchValueTareButton > touchTareTreshold) {
+    previousTouch = 0;
+    PowerOn = false;
+    Tare = false;
+    Unit = false;
+  }
+
+  
 }
 
 
