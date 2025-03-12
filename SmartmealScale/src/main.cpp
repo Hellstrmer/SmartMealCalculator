4e7908354d169811f11030c45c35c166a1cd2b12
Hellstrmer <jesper.hellstrm@gmail.com>
2025-03-12 20:06:23 +0100

    Fixed Tarefunction

--- SmartmealScale/src/main.cpp
+++ SmartmealScale/src/main.cpp
@@ -5,7 +5,7 @@
 #include <HTTPClient.h>
 #include <HX711.h>
 
-//Buttons
+// Buttons
 
 const int touchPin = T7;
 const int touchPowerTreshold = 80;
@@ -20,12 +20,11 @@ bool PowerOn;
 bool Tare;
 bool Unit;
 
-
-//SignalR
+// SignalR
 WebSocketsClient client;
 String handshake = "{\"protocol\":\"json\",\"version\":1}";
 
-//HX711
+// HX711
 const int LoadCell_DOUT_Pin = 17;
 const int LoadCell_SCK_Pin = 16;
 long TareOffset = 0;
@@ -33,148 +32,183 @@ long Weight = 0;
 float calibration_factor = 1;
 HX711 scale;
 
-//Delay
+// Delay
 unsigned long previousMillis = 0;
+unsigned long previousMillisTare = 0;
 const long interval = 500;
 
-void WebsocketEvent(WStype_t type, uint8_t* payload, size_t length){
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.println("Disconnected from WebSocket server");
-      break;
-    case WStype_CONNECTED:
-      Serial.println("Connected to WebSocket server");      
-      client.sendTXT(handshake);
-      delay(1000);
-      
-      break;
-    case WStype_TEXT:
-      Serial.printf("Received message: %s\n", payload);
-      break;
-  }
-}
+//Tare
+enum TareState {
+  Tare_Idle,
+  Tare_Wait,
+  Tare_Read
+};
+TareState tareState = Tare_Idle;
+unsigned long tareStartTime = 0;
+const int TareCount = 5;
+int CurrentIndex = 0;
+long sum = 0;
 
-void SendMessage(String message){
-  String SignalRMessage = String("{\"arguments\":[\"") + message + "\"],\"invocationId\":\"0\",\"target\":\"SendMessage\",\"type\":1}";
-  client.sendTXT(SignalRMessage);
-}
-
-void SendWeight(string barcode, int weight){
-  String SignalRMessage = String("{\"arguments\":[\"") + barcode +  weight "\"],\"invocationId\":\"0\",\"target\":\"SendWeightData\",\"type\":1}";
-  client.sendTXT(SignalRMessage);
-}
-
-void TareWeight(){
-
-  TareOffset = scale.read();
-}
-
-long Readweight(){
-  long rawValue = scale.read() - TareOffset;
-  
-  if (rawValue > 0) {
-    Weight = rawValue * calibration_factor;
-    return Weight;
-  } 
-  return 0;   
-}
-
-void CalibrateWeight(){
-  calibration_factor = 1010.0 / 314674.0;
-  Serial.println("Calibrated!");
-}
-
-void ReadButtons(){
-  int touchValuePowerButton = touchRead(touchPin);
-  int touchValueUnitButton = touchRead(UnitPin);
-  int touchValueTareButton = touchRead(TarePin);
-
-
-  if (touchValuePowerButton < touchPowerTreshold && previousTouchPower == 0){
-    PowerOn = true;
-    SendMessage("Startad!");
-    previousTouchPower = touchValuePowerButton;
-  } 
-  else if (touchValueUnitButton < touchUnitTreshold && previousTouchUnit == 0){    
-    Unit = true;
-    SendMessage("Unit");
-    CalibrateWeight();
-    previousTouchUnit = touchValueUnitButton;
-  } 
-  else if (touchValueTareButton < touchTareTreshold && previousTouchTare == 0){
-    Tare = true;    
-    TareWeight();
-    SendMessage("Tare");
-    Serial.println(Readweight());
-
-    previousTouchTare = touchValueTareButton;
-  } else if (touchValuePowerButton > touchPowerTreshold
-    && touchValueUnitButton > touchUnitTreshold  
-    && touchValueTareButton > touchTareTreshold) {
-    previousTouchPower = 0;
-    previousTouchUnit = 0;
-    previousTouchTare = 0;
-    PowerOn = false;
-    Tare = false;
-    Unit = false;
-  }  
-}
-
-
-
-void setup()
+void StartWifi()
 {
-  //Wifi
-  Serial.begin(9600);
-  delay(1000);
-
-  // Serial.println("Activated deep sleep..");
-
-  // esp_sleep_enable_touchpad_wakeup();
   WiFi.begin(ssid, password);
 
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(1000);
+    delay(500);
     Serial.print(".");
   }
   Serial.println("\nConnected to Wi-Fi!");
+}
 
-//SignalR
-  client.begin("192.168.50.51", 5099, "/weight");
-  client.onEvent(WebsocketEvent);
 
-//HX711
-scale.begin(LoadCell_DOUT_Pin, LoadCell_SCK_Pin);
-TareWeight();
-CalibrateWeight();
+void WebsocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.println("Disconnected from WebSocket server");
+    break;
+  case WStype_CONNECTED:
+    Serial.println("Connected to WebSocket server");
+    client.sendTXT(handshake);
+    delay(300);
+    break;
+  }
+}
+
+void SendMessage(String message)
+{
+  String SignalRMessage = String("{\"arguments\":[\"") + message + "\"],\"invocationId\":\"0\",\"target\":\"SendMessage\",\"type\":1}";
+  client.sendTXT(SignalRMessage);
+}
+
+void SendWeight(String barcode, int weight)
+{
+  String SignalRMessage = "{\"arguments\":[\"" + barcode + "\"," + String(weight) + "],\"invocationId\":\"0\",\"target\":\"SendWeightData\",\"type\":1}";
+  client.sendTXT(SignalRMessage);
+}
+
+long Readweight()
+{
+  long rawValue = scale.read() - TareOffset;
+   return rawValue * calibration_factor;
+}
+
+void TareWeight()
+{
+  switch(tareState) {
+    case Tare_Idle:
+    if (Tare) {
+      tareState = Tare_Wait;
+      tareStartTime = millis();
+      break;
+    }
+    break;
+    
+    case Tare_Wait:
+    if (millis() - tareStartTime >= 500) {
+      tareState = Tare_Read;
+    }
+    break;
+
+    case Tare_Read:
+    if (CurrentIndex < TareCount)
+    {
+      sum += scale.read();
+      CurrentIndex += 1;
+    } else 
+    {
+      TareOffset = sum / TareCount;
+      CurrentIndex = 0;
+      sum = 0;
+      Tare = false;
+      tareState = Tare_Idle;
+      Serial.println("Tare!");
+    }
+    break;
+  }
+}
+
+void CalibrateWeight()
+{
+  calibration_factor = 1010.0 / 314674.0;
+  Serial.println("Calibrated!");
+}
+
+void CheckTouchButtons(int pin, int threshold, bool &state, int &previousState, String message, void (*callback)() = nullptr)
+{
+  int touchValue = touchRead(pin);
+  if (touchValue < threshold && previousState == 0)
+  {
+    state = true;
+    SendMessage(message);
+    if (callback)
+    {
+      previousMillisTare = millis();
+      callback();
+    }
+    previousState = touchValue;
+  }
+  else if (touchValue > threshold)
+  {
+    previousState = 0;
+    state = false;
+  }
+}
+
+void ReadButtons()
+{
+  CheckTouchButtons(touchPin, touchPowerTreshold, PowerOn, previousTouchPower, "Startad!");
+  CheckTouchButtons(UnitPin, touchUnitTreshold, Unit, previousTouchUnit, "Unit", CalibrateWeight);
+  CheckTouchButtons(TarePin, touchTareTreshold, Tare, previousTouchTare, "Tare", TareWeight);
+}
+
+void ProcessWeight()
+{
+  long weight = Readweight();
+  // unsigned long currentMillis = millis();
+  // if (currentMillis - previousMillis >= interval)
+  // {
+  //   previousMillis = currentMillis;
+  //   if (scale.wait_ready_timeout(1000))
+  //   {
+  //     Serial.print("HX711 reading: ");
+  //     Serial.println(weight);
+  //   }
+  // }
+  SendWeight("ESP", weight);
+}
+
+
+void StartSignalR()
+{
+client.begin("192.168.50.51", 5099, "/weight");
+client.onEvent(WebsocketEvent);
+}
+
+void StartScale()
+{
+  scale.begin(LoadCell_DOUT_Pin, LoadCell_SCK_Pin);
+  Tare = true;
+  TareWeight();
+  CalibrateWeight();
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(100);
+  StartWifi();
+  StartSignalR();
+  StartScale();
 }
 
 void loop()
 {
-
   client.loop();
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval){
-    previousMillis = currentMillis;
-    static int messageCount = 0;
-    if (messageCount == 0) {
-      if (scale.wait_ready_timeout(1000)) {
-        Serial.print("HX711 reading: ");
-        Serial.println(Readweight());
-      } else {
-        Serial.println("HX711 not found.");
-      }
-    }
-    // } else if (messageCount == 1) {
-    //   analogWrite(displayPin, 0);
-    // }
-   messageCount = (messageCount + 1) % 2;
- }  
-  
   ReadButtons();
-  SendWeight("ESP", Readweight());
+  ProcessWeight();
+  TareWeight();
 }
-
-
